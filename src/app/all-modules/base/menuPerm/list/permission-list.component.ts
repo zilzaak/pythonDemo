0318c47ac98c2586d31ee13e3c8590a39fc5a192
paarvez miah <31301471+zilzaak@users.noreply.github.com>
2025-07-20 18:02:03 +0600

    --
    --

--- src/app/all-modules/base/menuPerm/list/permission-list.component.ts
+++ src/app/all-modules/base/menuPerm/list/permission-list.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { CommonServiceService } from 'src/app/all-modules/commonService/common-service.service';
 
 @Component({
   selector: 'app-permission-list',
@@ -7,9 +9,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PermissionListComponent implements OnInit {
 
-  constructor() { }
+    listData:any;
+    searchForm!:FormGroup;
+    formBuilder!:FormBuilder;
+    role:any;
+
+   constructor(private commonService:CommonServiceService,
+    private fromBuilder:FormBuilder
+   ) {
+
+
+   }
+
 
   ngOnInit(): void {
+   this.initializeForm();
+   this.loadRoles();
   }
+
+    initializeForm(){
+      this.searchForm = this.formBuilder.group({
+        user: [''],
+        role: ['']
+      });
+    }
+
+    loadRoles(){
+
+    }
 
 }
