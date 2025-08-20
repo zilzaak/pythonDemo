3e8270093c05cf26ae47f31c0770eb698f883c3e
paarvez miah <31301471+zilzaak@users.noreply.github.com>
2025-08-20 17:54:46 +0600

    --
    --

--- src/app/all-modules/base/menuPerm/list/permission-list.component.ts
+++ src/app/all-modules/base/menuPerm/list/permission-list.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { CommonServiceService } from 'src/app/all-modules/commonService/common-service.service';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-permission-list',
@@ -9,33 +10,73 @@ import { CommonServiceService } from 'src/app/all-modules/commonService/common-s
 })
 export class PermissionListComponent implements OnInit {
 
-    listData:any;
-    searchForm!:FormGroup;
-    formBuilder!:FormBuilder;
-    role:any;
-
-   constructor(private commonService:CommonServiceService,
-    private fromBuilder:FormBuilder
-   ) {
-
-
-   }
-
+  listData:any[]=[];
+  searchForm!:FormGroup;
+  baseUrl=environment.baseUrl;
+  totalItems = 0;
+  totalPages = 0;
+  currentPage = 1;
+  pageSize = 10;
+  constructor(private commonService:CommonServiceService,    private formBuilder: FormBuilder,) { }
 
   ngOnInit(): void {
-   this.initializeForm();
-   this.loadRoles();
+ this.initForm();
+ this.loadPage(1);
   }
 
-    initializeForm(){
-      this.searchForm = this.formBuilder.group({
-        user: [''],
-        role: ['']
+  initForm(){
+
+    this.searchForm=this.formBuilder.group({
+      parentModule:[''],
+      childModule:[''],
+      apiPattern:['']
+    })
+  }
+
+  getPageRange(): number[] {
+    const maxVisiblePages = 5;
+    const start = Math.max(1, this.currentPage - Math.floor(maxVisiblePages / 2));
+    const end = Math.min(this.totalPages, start + maxVisiblePages - 1);
+    const range = [];
+  
+    for (let i = start; i <= end; i++) {
+      range.push(i);
+    }
+  
+    return range;
+  }
+
+  loadPage(page: number) {
+  if (page < 1 ) return;
+
+  this.currentPage = page;
+  const params = {
+    pageNum: page.toString(),
+    pageSize: this.pageSize.toString()
+  };
+    this.commonService.getWithToken('http://localhost:8000/base/permittedModule/list', params)
+      .subscribe({
+        next: (response) => {
+          this.listData = response.data.listData || [];
+          this.totalItems = response.data.totalItems;
+          this.totalPages = response.data.totalPages;
+          this.totalItems = response.data.totalItems;
+          this.pageSize = response.data.pageSize;
+        },
+        error: (err) => {
+          console.error('Failed to load module list', err);
+        }
       });
-    }
+  }
 
-    loadRoles(){
+  onSearch() {
+    this.loadPage(1); // Reset to page 1 on search
+  }
 
-    }
+  reset() {
+    this.searchForm.reset();
+    this.loadPage(1);
+  }
 
 }
+
