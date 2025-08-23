190e56e87a5ba5d916976ee36e897d679bbcd9c4
paarvez miah <31301471+zilzaak@users.noreply.github.com>
2025-08-23 13:05:23 +0600

    --
    --

--- src/app/all-modules/base/menuPerm/list/permission-list.component.ts
+++ src/app/all-modules/base/menuPerm/list/permission-list.component.ts
@@ -10,27 +10,42 @@ import { environment } from 'src/environments/environment';
 })
 export class PermissionListComponent implements OnInit {
 
+
   listData:any[]=[];
-  searchForm!:FormGroup;
   baseUrl=environment.baseUrl;
   totalItems = 0;
   totalPages = 0;
   currentPage = 1;
   pageSize = 10;
-  constructor(private commonService:CommonServiceService,    private formBuilder: FormBuilder,) { }
+
+
+  loading = false;
+  loadingDropdown = false;
+  currentPage1 = 1;
+  pageSize1 = 10;    
+  hasMore = true; 
+  menuOptions:any[]=[];
+  userOptions:any[]=[];
+  private debounceTimer: any;
+  roleList: any;
+  searchForm!: FormGroup;
+
+  constructor(private commonService:CommonServiceService,   
+     private formBuilder: FormBuilder,) { }
 
   ngOnInit(): void {
  this.initForm();
+ this.fetchRoles();
  this.loadPage(1);
   }
 
   initForm(){
 
     this.searchForm=this.formBuilder.group({
-      parentModule:[''],
-      childModule:[''],
-      apiPattern:['']
-    })
+      userId:[''],
+      roleId:[''],
+      menuId:['']
+    });
   }
 
   getPageRange(): number[] {
@@ -46,14 +61,48 @@ export class PermissionListComponent implements OnInit {
     return range;
   }
 
+  getList(){
+  this.loadPage(this.currentPage);
+  }
+
+  fetchRoles() {
+    let api=this.baseUrl+"/base/role/list"
+    this.commonService.getWithToken(api,{}).subscribe(
+      { next: (response) => {
+        this.roleList = response?.data?.listData;
+        console.log("==========================")
+        console.log(this.roleList)
+        },
+        error: (err) => {
+        }
+      }
+    )
+  }
+
   loadPage(page: number) {
   if (page < 1 ) return;
 
   this.currentPage = page;
-  const params = {
+  let params:any = {
     pageNum: page.toString(),
     pageSize: this.pageSize.toString()
   };
+
+  if(this.searchForm.value.roleId && this.searchForm.value.roleId!=null && this.searchForm.value.roleId!==''){
+    params.roleId=this.searchForm.value.roleId;
+  }
+  if(this.searchForm.value.userId && this.searchForm.value.userId!=null && this.searchForm.value.userId!==''){
+    params.userId=this.searchForm.value.userId;
+  }
+  if(this.searchForm.value.menuId && this.searchForm.value.menuId!=null && this.searchForm.value.menuId!==''){
+    params.menuId=this.searchForm.value.menuId;
+  }
+
+  console.log("search form vaue is ");
+  console.log(this.searchForm.value);
+  console.log("params vaue is ");
+  console.log(params);
+
     this.commonService.getWithToken('http://localhost:8000/base/permittedModule/list', params)
       .subscribe({
         next: (response) => {
@@ -69,13 +118,87 @@ export class PermissionListComponent implements OnInit {
       });
   }
 
-  onSearch() {
-    this.loadPage(1); // Reset to page 1 on search
-  }
-
   reset() {
     this.searchForm.reset();
     this.loadPage(1);
+  }
+
+
+  onSearch(event: any,menu:any): void {
+    const term = event.term?.trim();
+    if (this.debounceTimer) {
+      clearTimeout(this.debounceTimer);
+    }
+    if (!term || term.length === 0) {
+      if(menu==='menu'){
+        this.menuOptions = [];
+      }
+           return;
+    }
+    this.debounceTimer = setTimeout(() => {
+      if(menu==='menu'){
+        this.currentPage1 = 1;
+        this.hasMore = true;
+      }
+      this.performSearch(term,menu);
+    }, 300);
+  }
+
+
+  loadMore(menu:any): void {
+    if (!this.hasMore || this.loadingDropdown) return;
+
+    const term = this.searchForm.get('menu')?.value?.trim();
+    if (!term || typeof term !== 'string') return;
+    this.currentPage1++;
+    this.loadingDropdown = true;
+   let params: any={ menu: term,  
+    pageNum: this.currentPage1.toString(),
+     pageSize: this.pageSize1.toString(),
+     loadMethod:"loadMethod"
+    }
+
+    let uri=this.baseUrl+"/base/module/list";
+    this.commonService.getWithToken(uri, params).subscribe({
+      next: (response) => {
+        if(menu==='menu'){
+          this.menuOptions = response?.data?.listData || [];
+          this.hasMore = this.menuOptions.length === this.pageSize1;
+        }
+        this.loadingDropdown = false;
+      },
+      error: (err) => {
+        this.loadingDropdown = false; 
+      }
+    });
+  }
+
+
+
+  private performSearch(term: string,menu:any): void {
+        if(menu==='menu'){
+      this.loadingDropdown = true;
+    }
+
+    let uri=this.baseUrl+"/base/module/list";
+    let params: any={
+      menu: term,
+      loadMethod:"loadMethod",
+      pageNum: this.currentPage.toString(),
+      pageSize: this.pageSize.toString()
+    }
+    this.commonService.getWithToken(uri, params).subscribe({
+      next: (response) => {
+        if(menu==='menu'){
+          this.menuOptions = response?.data?.listData || [];
+          this.hasMore = this.menuOptions.length === this.pageSize;
+          this.loadingDropdown = false; 
+        }
+      },
+      error: (err) => {
+        this.loadingDropdown = false; 
+      }
+    });
   }
 
 }
