8c9e83efbcd6b2ff64aafbc64606bf05bfc5a613
paarvez miah <31301471+zilzaak@users.noreply.github.com>
2025-08-24 18:40:15 +0600

    --
    --

--- src/app/all-modules/base/user/create/user-create.component.ts
+++ src/app/all-modules/base/user/create/user-create.component.ts
@@ -58,6 +58,7 @@ export class UserCreateComponent implements OnInit {
 
   initializeForm() {
     this.createForm = this.formBuilder.group({
+      id:[''],
       enabled: [true],
       email: [''],
       username:[''],
