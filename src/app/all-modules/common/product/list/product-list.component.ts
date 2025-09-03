d3a1d3a27a79651db73fc8b9dd471bdd5d9e898c
paarvez miah <31301471+zilzaak@users.noreply.github.com>
2025-09-03 12:27:18 +0600

    -
    --

--- src/app/all-modules/common/product/list/product-list.component.ts
+++ src/app/all-modules/common/product/list/product-list.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormBuilder } from '@angular/forms';
 import { CommonServiceService } from 'src/app/all-modules/commonService/common-service.service';
+import { LoginServiceService } from 'src/app/login-module/service/login-service.service';
 import { environment } from 'src/environments/environment';
 
 @Component({
@@ -17,7 +18,7 @@ export class ProductListComponent implements OnInit {
   totalPages = 0;
   currentPage = 1;
   pageSize = 10;
-
+  private debounceTimer: any;
   roleList: any;
   username:any;
 
@@ -40,12 +41,47 @@ export class ProductListComponent implements OnInit {
   private debounceTimerm: any;
 
 
+  loadingDropdown = false;
+  barndLoadingDropDown = false;
+  modelLoadingDropDown = false;
+  catLoadingDropDown = false;
+  colorLoadingDropDown = false;
+
+  sizeLoadingDropDown = false;
+  madeLoadingDropDown = false;
+  uomLoadingDropDown = false;
+
+  hasMore = true;
+  brandHasMore = true;
+  modelHasMore = true;
+  catHasMore = true;
+  colorHasMore = true;
+  sizeHasMore = true;
+  uomHasMore = true;
+  madeHasMore = true;
+
+  catOptions: any[] = [];
+  modelOptions: any[] = [];
+  colorOptions: any[] = [];
+  sizeOptions: any[] = [];
+  oumOptions: any[] = [];
+  madeWithOptions: any[] = [];
+  brandOptions:any[]=[];
+
+  searchItem:any;
+
   uvalue:any;
   mvalue:any;
 
-  constructor(private commonService:CommonServiceService,    private formBuilder: FormBuilder,) { }
+  constructor(private commonService:CommonServiceService,  
+      private formBuilder: FormBuilder,
+    private loginService:LoginServiceService) { }
 
-  ngOnInit(): void {
+  async ngOnInit(): Promise<void> {
+ let m:any=this.loginService.userInfo();
+ let userData:any;
+ userData = await this.loginService.userInfo();
+ this.menuOptions=userData.orgList;
  this.initForm();
  this.loadPage(1);
   }
@@ -53,9 +89,11 @@ export class ProductListComponent implements OnInit {
   initForm(){
 
     this.searchForm=this.formBuilder.group({
-      parentMenuId:[''],
-      backendUrl:[''],
-      frontendUrl:['']
+      name:[''],
+      catId:[''],
+      modelId:[''],
+      brandId:[''],
+      orgId:[Number(localStorage.getItem('orgId'))]
     })
   }
 
@@ -72,6 +110,9 @@ export class ProductListComponent implements OnInit {
     return range;
   }
 
+  setSearch(x: any) {
+    this.searchItem = x.value;
+  }
   loadPage(page: number) {
   if (page < 1 ) return;
 
@@ -83,26 +124,30 @@ export class ProductListComponent implements OnInit {
   };
   console.log("form value is ");
   console.log( this.searchForm.value);
-  if(this.searchForm.value.parentMenuId && this.searchForm.value.parentMenuId!=null 
-    && this.searchForm.value.parentMenuId!==''
+  if(this.searchForm.value.modelId && this.searchForm.value.modelId!=null && this.searchForm.value.modelId!==''
   ){
-    params.parentMenuId=this.searchForm.value.parentMenuId;
+    params.modelId=this.searchForm.value.modelId;
   }
 
-  if(this.searchForm.value.backendUrl && this.searchForm.value.backendUrl!=null 
-    && this.searchForm.value.backendUrl!==''
+  if(this.searchForm.value.catId && this.searchForm.value.catId!=null && this.searchForm.value.catId!==''
   ){
-    params.backendUrlId=this.searchForm.value.backendUrl;
+    params.catId=this.searchForm.value.catId;
   }
 
-  if(this.searchForm.value.frontendUrl && this.searchForm.value.frontendUrl!=null 
-    && this.searchForm.value.frontendUrl!==''
+  if(this.searchForm.value.brandId && this.searchForm.value.brandId!=null  && this.searchForm.value.brandId!==''
   ){
-    params.frontendUrl=this.searchForm.value.frontendUrl;
+    params.brandId=this.searchForm.value.brandId;
   }
 
-  params.orgId=Number(localStorage.getItem('orgId'));
+  if(this.searchForm.value.orgId && this.searchForm.value.orgId!=null && this.searchForm.value.orgId!==''
+  ){
+    params.orgId=this.searchForm.value.orgId;
+  }
 
+  if(this.searchForm.value.name && this.searchForm.value.name!=null && this.searchForm.value.name!==''
+  ){
+    params.name=this.searchForm.value.name;
+  }
     this.commonService.getWithToken('http://localhost:8000/setting/product/list', params)
       .subscribe({
         next: (response) => {
@@ -306,7 +351,7 @@ error: (err) => {
         let formData:any={
           id:x
         };
-          this.commonService.sendDeleteRequest(this.baseUrl+"/base/module/delete",formData).subscribe({
+          this.commonService.sendDeleteRequest(this.baseUrl+"/setting/product/delete",formData).subscribe({
           next: (response: any) => {
             if (response.success) {
               alert(response.message);
@@ -320,5 +365,242 @@ error: (err) => {
         });
 
       }
+
+
+      
+  onBrandSearch(event: any, entity: any): void {
+    const term = event.term?.trim();
+    if (this.debounceTimer) {
+      clearTimeout(this.debounceTimer);
+    }
+    if (!term || term.length === 0) {
+      if (entity === 'Brand') {
+        this.brandOptions = [];
+      } else if (entity === 'ProductModel') {
+        this.modelOptions = [];
+      }
+      else if (entity === 'ProductCat') {
+        this.catOptions = [];
+      }
+      else if (entity === 'ProductColor') {
+        this.colorOptions = [];
+      }
+      else if (entity === 'ProductSize') {
+        this.sizeOptions = [];
+      }
+      else if (entity === 'MadeWith') {
+        this.madeWithOptions = [];
+      }
+      else if (entity === 'UnitOfMeasure') {
+        this.oumOptions = [];
+      }
+      return;
+    }
+    this.debounceTimer = setTimeout(() => {
+      this.currentPage = 1;
+      this.brandHasMore = true;
+      this.brandPerformSearch(term, entity);
+    }, 300);
+  }
+
+  brandLoadMore(entity: any): void {
+
+    if (entity === 'Brand') {
+      if (!this.brandHasMore || this.barndLoadingDropDown) {
+        return;
+      } else {
+        this.barndLoadingDropDown = true;
+      }
+    } else if (entity === 'ProductModel') {
+      if (!this.modelHasMore || this.modelLoadingDropDown) {
+        return;
+      } else {
+        this.modelLoadingDropDown = true;
+      }
+    }
+    else if (entity === 'ProductCat') {
+      if (!this.catHasMore || this.catLoadingDropDown) {
+        return;
+      } else {
+        this.catLoadingDropDown = true;
+      }
+    }
+    else if (entity === 'ProductColor') {
+      if (!this.colorHasMore || this.colorLoadingDropDown) {
+        return;
+      } else {
+        this.colorLoadingDropDown = true;
+      }
+    }
+    else if (entity === 'ProductSize') {
+      if (!this.sizeHasMore || this.sizeLoadingDropDown) {
+        return;
+      } else {
+        this.sizeLoadingDropDown = true;
+      }
+    }
+    else if (entity === 'MadeWith') {
+      if (!this.madeHasMore || this.madeLoadingDropDown) {
+        return;
+      } else {
+        this.madeLoadingDropDown = true;
+      }
+    }
+    else if (entity === 'UnitOfMeasure') {
+      if (!this.uomHasMore || this.uomLoadingDropDown) {
+        return;
+      } else {
+        this.uomLoadingDropDown = true;
+      }
+    }
+
+    const term = this.searchItem;
+    if (!term || typeof term !== 'string') return;
+    this.currentPage++;
+
+    let params: any = {
+      name: term,
+      entity: entity,
+      pageNum: this.currentPage.toString(),
+      pageSize: this.pageSize.toString()
+    }
+
+    let uri = this.baseUrl + "/setting/productCriteria/list";
+    this.commonService.getWithToken(uri, params).subscribe({
+      next: (response) => {
+        if (entity === 'Brand') {
+          this.brandOptions = response?.data?.listData;
+          this.barndLoadingDropDown = false;
+        } else if (entity === 'ProductModel') {
+          this.modelOptions = response?.data?.listData;
+          this.modelLoadingDropDown = false;
+        }
+        else if (entity === 'ProductCat') {
+          this.catOptions = response?.data?.listData;
+          this.catLoadingDropDown = false;
+        }
+        else if (entity === 'ProductColor') {
+          this.colorOptions = response?.data?.listData;
+          this.colorLoadingDropDown = false;
+        }
+        else if (entity === 'ProductSize') {
+          this.sizeOptions = response?.data?.listData;
+          this.sizeLoadingDropDown = false;
+        }
+        else if (entity === 'MadeWith') {
+          this.madeWithOptions = response?.data?.listData;
+          this.madeLoadingDropDown = false;
+        }
+        else if (entity === 'UnitOfMeasure') {
+          this.oumOptions = response?.data?.listData;
+          this.uomLoadingDropDown = false;
+        }
+      },
+      error: (err) => {
+        if (entity === 'Brand') {
+          this.barndLoadingDropDown = false;
+
+        } else if (entity === 'ProductModel') {
+          this.modelLoadingDropDown = false;
+        }
+        else if (entity === 'ProductCat') {
+          this.catLoadingDropDown = false;
+        }
+        else if (entity === 'ProductColor') {
+          this.colorLoadingDropDown = false;
+        }
+        else if (entity === 'ProductSize') {
+          this.sizeLoadingDropDown = false;
+
+        }
+        else if (entity === 'MadeWith') {
+          this.madeLoadingDropDown = false;
+        }
+        else if (entity === 'UnitOfMeasure') {
+          this.uomLoadingDropDown = false;
+        }
+      }
+    });
+  }
+
+  private brandPerformSearch(term: string, entity: any): void {
+    if (entity === 'Brand') {
+      this.barndLoadingDropDown = true;
+    } else if (entity === 'ProductModel') {
+      this.modelLoadingDropDown = true;
+    }
+    else if (entity === 'ProductCat') {
+      this.catLoadingDropDown = true;
+    }
+    else if (entity === 'ProductColor') {
+      this.colorLoadingDropDown = true;
+    }
+    else if (entity === 'ProductSize') {
+      this.sizeLoadingDropDown = true;
+    }
+    else if (entity === 'MadeWith') {
+      this.madeLoadingDropDown = true;
+    }
+    else if (entity === 'UnitOfMeasure') {
+      this.uomLoadingDropDown = true;
+    }
+
+    let uri = this.baseUrl + "/setting/productCriteria/list";
+    let params: any = {
+      entity: entity,
+      pageNum: this.currentPage.toString(),
+      pageSize: this.pageSize.toString(),
+      name: this.searchItem,
+      orgId: this.searchForm.value.orgId
+    }
+    this.commonService.getWithToken(uri, params).subscribe({
+      next: (response) => {
+        if (entity === 'Brand') {
+          this.brandOptions = response?.data?.listData;
+          this.brandHasMore = this.brandOptions.length === this.pageSize;
+          this.barndLoadingDropDown = false;
+        } else if (entity === 'ProductModel') {
+          this.modelOptions = response?.data?.listData;
+          this.modelHasMore = this.modelOptions.length === this.pageSize;
+          this.modelLoadingDropDown = false;
+        }
+        else if (entity === 'ProductCat') {
+          this.catOptions = response?.data?.listData;
+          this.catHasMore = this.catOptions.length === this.pageSize;
+          this.catLoadingDropDown = false;
+        }
+        else if (entity === 'ProductColor') {
+          this.colorOptions = response?.data?.listData;
+          this.colorHasMore = this.colorOptions.length === this.pageSize;
+          this.colorLoadingDropDown = false;
+        }
+        else if (entity === 'ProductSize') {
+          this.sizeOptions = response?.data?.listData;
+          this.sizeHasMore = this.sizeOptions.length === this.pageSize;
+          this.sizeLoadingDropDown = false;
+        }
+        else if (entity === 'MadeWith') {
+          this.madeWithOptions = response?.data?.listData;
+          this.madeHasMore = this.madeWithOptions.length === this.pageSize;
+          this.madeLoadingDropDown = false;
+        }
+        else if (entity === 'UnitOfMeasure') {
+          this.oumOptions = response?.data?.listData;
+          this.uomHasMore = this.oumOptions.length === this.pageSize;
+          this.uomLoadingDropDown = false;
+        }
+      },
+      error: (err:any) => {
+        this.barndLoadingDropDown = false;
+        this.modelLoadingDropDown = false;
+        this.catLoadingDropDown = false;
+        this.colorLoadingDropDown = false;
+        this.sizeLoadingDropDown = false;
+        this.madeLoadingDropDown = false;
+        this.uomLoadingDropDown = false;
+
+      }
+    });
+  }
 
 }
