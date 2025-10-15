fc9495c1ebcb59ff123070f0ab94c4ad0665aab2
paarvez miah <31301471+zilzaak@users.noreply.github.com>
2025-10-15 18:01:12 +0600

    supplier added
    supplier added

--- src/app/all-modules/purchase/purchase-routing.module.ts
+++ src/app/all-modules/purchase/purchase-routing.module.ts
@@ -2,6 +2,8 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { PurchListComponent } from './purchList/purch-list.component';
 import { PurchCrudComponent } from './purchCrud/purch-crud.component';
+import { SupplierListComponent } from './supplier/suppList/supplier-list.component';
+import { SuppCrudComponent } from './supplier/suppCrud/supp-crud.component';
 
 const routes: Routes = [
      { path: 'product/list',component: PurchListComponent,
@@ -18,7 +20,25 @@ const routes: Routes = [
       {
         path: 'product/view/:id', component: PurchCrudComponent,
         data: { title: 'View' },
-      }];
+      },
+      {
+        path: 'supplier/list', component: SupplierListComponent,
+        data: { title: 'List' }
+      },
+      {
+        path: 'supplier/edit/:id', component: SuppCrudComponent,
+        data: { title: 'Edit' }
+      },
+      {
+        path: 'supplier/view/:id', component: SuppCrudComponent,
+        data: { title: 'View' }
+      },
+      {
+        path: 'supplier/create', component: SuppCrudComponent,
+        data: { title: 'Create' }
+      },
+
+    ];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
--- src/app/all-modules/purchase/supplier/suppCrud/supp-crud.component.ts
+++ src/app/all-modules/purchase/supplier/suppCrud/supp-crud.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { FormGroup, FormBuilder, Validators, FormArray } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
+import { CommonServiceService } from 'src/app/all-modules/commonService/common-service.service';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-supp-crud',
@@ -7,9 +11,308 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SuppCrudComponent implements OnInit {
 
-  constructor() { }
+  @ViewChild('myButton') myButton!: ElementRef;
+
+  similarProduct: any;
+  selectForm!: FormGroup;
+  purchaseForm!: FormGroup;
+  createForm!: FormGroup;
+  pageTitle!: any;
+  opMode!: any;
+  api!: any;
+  baseUrl = environment.baseUrl;
+  loading = false;
+  listData: any[] = [];
+  loadDropOrg = false;
+  loadDropSupp = false;
+  loadDropPro = false;
+  currentPage = 1;
+  pageSize = 10;
+  hasMore = true;
+  hasMoreSupp = true;
+  hasMorePro = true;
+  menuOptions: any[] = [];
+  products: any[] = [];
+  suppList: any[] = [];
+  private debounceTimer: any;
+  searchItem: any;
+  invList: any[] = [];
+  madeWithOptions: any[] = [];
+  branchList: any[] = [];
+  id: any;
+  stocks:any[]=[];
+   priceSum:any=0;
+   totalNetPrice:any=0;
+   pricePayable:any=0; 
+   totalDiscount:any=0;
+   totalVat:any=0;
+  constructor(
+    private commmonService: CommonServiceService,
+    private formBuilder: FormBuilder,
+    private activeRouter: ActivatedRoute,
+    private router: Router
+  ) { }
 
   ngOnInit(): void {
+    this.stocks.push(50,50,450,120,22,45);
+    this.initializeForm();
+    this.pageTitle = this.activeRouter.snapshot.data['title'];
+
+    if (this.pageTitle === 'Create') {
+      this.menuOptions = [
+        {
+          id: Number(localStorage.getItem('orgId')),
+          orgName: localStorage.getItem('orgName')
+        }
+      ];
+      this.opMode = 'create';
+      this.api = this.baseUrl + '/setting/product/create2';
+    } else if (this.pageTitle === 'Edit') {
+      this.opMode = 'edit';
+      this.api = this.baseUrl + '/setting/product/update';
+      this.formData(this.activeRouter.snapshot.params.id);
+    } else if (this.pageTitle === 'View') {
+      this.opMode = 'view';
+      this.api = this.baseUrl + '/setting/product/list';
+      this.formData(this.activeRouter.snapshot.params.id);
+    }
   }
+
+
+  ngAfterViewInit() {
+    console.log('Button element:', this.myButton.nativeElement);
+  }
+
+  formData(id: any) {
+    this.id = id;
+    let org: any = localStorage.getItem('orgId');
+    const para = { id: id, orgId: org };
+    this.commmonService.getWithToken(this.baseUrl + '/purchase/product/list', para).subscribe({
+      next: (response) => {
+        this.menuOptions = [
+          {
+            id: Number(localStorage.getItem('orgId')),
+            orgName: localStorage.getItem('orgName')
+          }
+        ];
+        let resp: any = response.data.listData[0];
+        this.createForm.patchValue(resp);
+        if (this.opMode === 'view') {
+          this.createForm.disable();
+          this.purchaseForm.disable();
+        }
+      },
+      error: (err) => {
+        console.error('Failed to load data', err);
+      }
+    });
+  }
+
+  initializeForm() {
+    this.purchaseForm = this.formBuilder.group(
+      {
+        purchPrices: this.formBuilder.array([this.sellPriceCreate(null)])
+      }
+    );
+  }
+
+  sellPriceCreate(obj:any): FormGroup {
+    if(obj==null){
+      return this.formBuilder.group({
+        id:[''],
+        orgId:['',Validators.required],
+        name:['',Validators.required],
+        phone:[''],
+        address:['',Validators.required],
+     })
+    }else{
+      return this.formBuilder.group({
+        id:[''],
+        orgId:['',Validators.required],
+        name:['',Validators.required],
+        phone:[''],
+        address:['',Validators.required],
+     })
+    }
+
+  }
+  get allPurchs(): FormArray {
+    return this.purchaseForm.get("purchPrices") as FormArray;
+  }
+
+  addSell(index: number): void {
+    this.allPurchs.insert(index, this.sellPriceCreate(null));
+  }
+
+  removeSell(index: any): void {
+    this.allPurchs.removeAt(index);
+  }
+
+
+  confirmSimilarity() {
+    this.createForm.controls['confirmSimilarity'].setValue(true);
+    this.onSubmit();
+  }
+
+  onSubmit() {
+    if (this.opMode === 'view') return;
+    if (this.createForm.invalid) {
+      alert('Invalid form');
+      return;
+    }
+    this.loading = true;
+    let payload: any = { ...this.createForm.value };
+
+    let method = this.opMode === 'create' ? 'post' : 'put';
+    this.commmonService.sendPostPutReq<any>(this.api.toString(), payload, method).subscribe({
+      next: (response: any) => {
+        if (response.success) {
+          alert(response.message);
+          this.router.navigate(['/purchase/supplier/list']);
+        } else {
+          this.loading = false;
+          if (response.message.includes('Similar')) {
+            this.similarProduct = response.data.existsData;
+            this.myButton.nativeElement.click();
+          } else {
+            alert(response.message);
+          }
+        }
+      },
+      error: () => {
+        this.loading = false;
+      }
+    });
+  }
+
+  setSearch(x: any) {
+    this.searchItem = x.value;
+  }
+
+
+  onSearch(entity: any): void {
+    const term = this.searchItem;
+    if (this.debounceTimer) {
+      clearTimeout(this.debounceTimer);
+    }
+
+    this.debounceTimer = setTimeout(() => {
+      this.currentPage = 1;
+      if (entity === 'Organization') {
+        this.hasMore = true;
+      }
+      if (entity === 'Supplier') {
+        this.hasMoreSupp = true;
+      }
+      if (entity === 'Product') {
+        this.hasMorePro = true;
+      }
+
+      this.performSearch(term, entity);
+    }, 300);
+  }
+
+  loadMore(entity: any): void {
+    const term = this.searchItem;
+    this.currentPage++;
+    let uri: string = '';
+
+    let params: any = {
+      commonField: term, 
+      dropDown: 'dropDown',entity: entity,
+      name:term,
+      orgId:this.createForm.value.orgId,
+      pageNum: this.currentPage.toString(),pageSize: this.pageSize.toString()
+    };
+
+    if (entity === 'Organization') {
+      this.loadDropOrg = true;
+      uri = this.baseUrl + '/base/organization/list';
+    }
+    if (entity === 'Supplier') {
+      this.loadDropSupp = true;
+      uri = this.baseUrl + '/purchase/supplier/list';
+    }
+    if (entity === 'Product') {
+      this.loadDropPro = true;
+      uri = this.baseUrl + '/setting/product/list';
+    }
+
+    this.commmonService.getWithToken(uri, params).subscribe({
+      next: (response) => {
+        if (entity === 'Organization') {
+          this.menuOptions = response?.data?.listData || [];
+          this.loadDropOrg = false;
+        }
+        if (entity === 'Supplier') {
+          this.suppList = response?.data?.listData || [];
+          this.loadDropSupp = false;
+        }
+        if (entity === 'Product') {
+          this.products = response?.data?.listData || [];
+          this.loadDropPro = false;
+        }
+
+      },
+      error: (err) => {
+        this.loadDropOrg = false;
+        this.loadDropSupp = false;
+        this.loadDropPro = false;
+      }
+    });
+  }
+
+  private performSearch(term: string, entity: any): void {
+    let uri: string = '';
+    if (entity === 'Organization') {
+      this.loadDropOrg = true;
+      uri = this.baseUrl + '/base/organization/list';
+    }
+    if (entity === 'Supplier') {
+      this.loadDropSupp = true;
+      uri = this.baseUrl + '/purchase/supplier/list';
+    }
+    if (entity === 'Product') {
+      this.loadDropPro = true;
+      uri = this.baseUrl + '/setting/product/list';
+    }
+
+    let params: any = {
+      commonField: term,
+      dropDown: 'dropDown',
+      pageNum: this.currentPage.toString(),
+      pageSize: this.pageSize.toString(),
+      entity: entity,
+      name:term,
+      orgId:this.createForm.value.orgId
+    };
+
+    this.commmonService.getWithToken(uri, params).subscribe({
+      next: (response) => {
+        if (entity === 'Organization') {
+          this.menuOptions = response?.data?.listData || [];
+          this.hasMore = this.menuOptions.length === this.pageSize;
+          this.loadDropOrg = false;
+        }
+        if (entity === 'Supplier') {
+          this.suppList = response?.data?.listData || [];
+          this.hasMoreSupp = this.menuOptions.length === this.pageSize;
+          this.loadDropSupp = false;
+        }
+        if (entity === 'Product') {
+          this.products = response?.data?.listData || [];
+          this.hasMorePro = this.products.length === this.pageSize;
+          this.loadDropPro = false;
+        }
+      },
+      error: (err) => {
+        this.loadDropOrg = false;
+        this.loadDropSupp = false;
+        this.loadDropPro = false;
+      }
+    });
+  }
+
+
 
 }
